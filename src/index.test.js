3467dbf0b122a263be687085086e50cd4388dde6
Marleono <73365729+Marleono@users.noreply.github.com>
2021-03-04 14:41:55 +0300

    Update index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,12 @@
 const pow = require('./index.js');
 
-test('multiply 2 3 times to equal 8', () => {
-  expect(pow(2, 3)).toBe(8);
+describe("test pow", () => {
+    it('multiply 2 3 times to equal 8', () => {
+        const result = pow(2, 3);
+        expect(result).toBe(8);
+    }),
+        it("multiply 3 3 times to equal 18", () => {
+            const result = pow(3, 3); // получаем результат выполнения функции
+            expect(result).toBe(18); // если результат оказался таким же как и то что внутри toBe(), то тест должен пройти успешно
+        })
 });
